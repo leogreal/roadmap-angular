d6dfc5e3233acd626c5a17736603c8f551c6712c
Leonardo Gregório de Almeida <leogreal@gmail.com>
2024-10-03 19:57:34 -0300

    Add angular material

--- 01-cards/src/app/cards/cards.module.ts
+++ 01-cards/src/app/cards/cards.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
+import { CardButtonCancelComponent } from './card-button-cancel/card-button-cancel.component';
 import { CardButtonComponent } from './card-button/card-button.component';
 import { CardComponent } from './card/card.component';
-import { CardButtonCancelComponent } from './card-button-cancel/card-button-cancel.component';
 
 @NgModule({
   declarations: [CardComponent, CardButtonComponent, CardButtonCancelComponent],
   exports: [CardComponent, CardButtonComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatSliderModule],
 })
 export class CardsModule {}
