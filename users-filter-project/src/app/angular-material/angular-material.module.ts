8831059ae0984a9e68ff2eb810b8305d4928c210
Leonardo Gregório de Almeida <leogreal@gmail.com>
2025-01-06 22:42:58 -0300

    add user-details

--- users-filter-project/src/app/angular-material/angular-material.module.ts
+++ users-filter-project/src/app/angular-material/angular-material.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+
+const importsExports = [
+  MatButtonModule,
+  MatDividerModule,
+  MatIconModule,
+  MatListModule,
+]
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule],
-  exports: [MatButtonModule, MatIconModule],
+  imports: importsExports,
+  exports: importsExports,
 })
 export class AngularMaterialModule {}
