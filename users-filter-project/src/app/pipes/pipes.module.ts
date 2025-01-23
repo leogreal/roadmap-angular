e092041cf9fd768976e4f1c2b8ceb115d3854456
Leonardo Gregório de Almeida <leogreal@gmail.com>
2025-01-22 21:30:31 -0300

    Add status pipe

--- users-filter-project/src/app/pipes/pipes.module.ts
+++ users-filter-project/src/app/pipes/pipes.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { AddressPipe } from "./address.pipe";
 import { PhonePipe } from "./phone.pipe";
+import { StatusPipe } from "./status.pipe";
 
 const declarationsExports = [
   PhonePipe,
   AddressPipe,
+  StatusPipe,
 ];
 
 @NgModule({
