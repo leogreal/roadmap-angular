1be4ff0f2cdc7848390465bb24adc465f17d7a77
Leonardo Gregório de Almeida <leogreal@gmail.com>
2025-01-22 21:36:53 -0300

    Use date pipe

--- users-filter-project/src/app/components/components.module.ts
+++ users-filter-project/src/app/components/components.module.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from "@angular/core";
 import { FormsModule } from '@angular/forms';
 import { AngularMaterialModule } from "../angular-material/angular-material.module";
+import { PipesModule } from "../pipes/pipes.module";
 import { UserDetailsComponent } from "./user-details/user-details.component";
 import { UserFiltersComponent } from "./user-filters/user-filters.component";
 import { UserListComponent } from "./user-list/user-list.component";
-import { PipesModule } from "../pipes/pipes.module";
 
 const declarationsExports = [
   UserDetailsComponent,
@@ -14,7 +15,7 @@ const declarationsExports = [
 
 @NgModule({
   declarations: declarationsExports,
-  imports: [AngularMaterialModule, FormsModule, PipesModule],
+  imports: [AngularMaterialModule, FormsModule, PipesModule, CommonModule],
   exports: declarationsExports,
 })
 export class ComponentsModule {}
