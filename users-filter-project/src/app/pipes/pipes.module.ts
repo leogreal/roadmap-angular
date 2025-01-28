2936984de4759703397ac2a186cb40333ba79565
Leonardo Gregório de Almeida <leogreal@gmail.com>
2025-01-27 21:16:56 -0300

    Add dashIfEmpty pipe

--- users-filter-project/src/app/pipes/pipes.module.ts
+++ users-filter-project/src/app/pipes/pipes.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { AddressPipe } from "./address.pipe";
+import { DashIfEmptyPipe } from "./dash-if-empty.pipe";
 import { PhonePipe } from "./phone.pipe";
 import { StatusPipe } from "./status.pipe";
 
@@ -7,6 +8,7 @@ const declarationsExports = [
   PhonePipe,
   AddressPipe,
   StatusPipe,
+  DashIfEmptyPipe,
 ];
 
 @NgModule({
